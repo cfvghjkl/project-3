c6f7f0c1789d28ca085cbe5c0483f683c6b19b3f
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-09-13 12:17:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,69 +1,19 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id 
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "resource-group1"
-  location = "Central India"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "virtual-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"  # Change from Dynamic to Static
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "VM1"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_B1s"
   admin_username      = "sagarika"
-  admin_password      = "Sagarika@123"
+  disable_password_authentication = true
+
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
+
+  admin_ssh_key {
+    username   = "sagarika"
+    public_key = file("~/.ssh/id_rsa.pub")  # Path to your public SSH key
+  }
 
   os_disk {
     caching              = "ReadWrite"
