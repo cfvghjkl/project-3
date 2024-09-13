4a1e3e76581022754a755d80972a896f90c3e6dc
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-09-13 17:15:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,13 +82,16 @@ resource "azurerm_linux_virtual_machine" "example" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
 
-  resource "null_resource" "run_ansible_playbook" {
+resource "null_resource" "run_ansible_playbook" {
   depends_on = [azurerm_linux_virtual_machine.example]
- 
+
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${azurerm_public_ip.example.ip_address},' install_nginx.yml --extra-vars='ansible_ssh_user=${var.username}' --private-key='/var/lib/jenkins/id_rsa' --become --become-user=root"
   }
 }
 
-
+output "public_ip" {
+  value = azurerm_public_ip.example.ip_address
+}
