71d6479f899554d5d552d9594bd74768f03d6deb
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-09-16 15:39:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,8 +85,14 @@ resource "azurerm_linux_virtual_machine" "example" {
  
 resource "null_resource" "run_ansible_playbook" {
   depends_on = [azurerm_linux_virtual_machine.example]
- 
+
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${azurerm_public_ip.example.ip_address},' install_nginx.yml --extra-vars='ansible_ssh_user=sagarika' --private-key='/var/lib/jenkins/id_rsa' --become --become-user=root"
+    command = <<EOT
+      ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${azurerm_public_ip.example.ip_address},' install_nginx.yml \
+      --extra-vars='ansible_ssh_user=sagarika' \
+      --private-key='/var/lib/jenkins/id_rsa' \
+      --become --become-user=root
+    EOT
   }
 }
+
